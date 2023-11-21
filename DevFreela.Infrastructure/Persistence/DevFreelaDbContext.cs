192f6cd428f27311bda12205e64f3c0514486c82
Matheus <matheuscnc4@gmail.com>
2023-11-20 22:11:45 -0300

    Criando DbSet e DbContext

--- DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
+++ DevFreela.Infrastructure/Persistence/DevFreelaDbContext.cs
@@ -2,40 +2,23 @@
 // No entanto, em um aplicativo real, esses dados fictícios geralmente seriam substituídos por dados reais armazenados em um banco de dados,
 // e métodos para acessar e manipular esses dados seriam implementados nesta classe.
 using DevFreela.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevFreela.Infrastructure.Persistence
 {
-    public class DevFreelaDbContext
+    public class DevFreelaDbContext : DbContext
     {
         // Criação de litas de novos usuários, projetos e skills, através de construtores que foram implementados na camada core.
-        public DevFreelaDbContext()
+        public DevFreelaDbContext(DbContextOptions<DevFreelaDbContext> options) : base(options)
         {
-            Projects = new List<Project>
-            {
-                new Project ("Meu projeto ASPNET Core 1 ", "Minha descrição de projeto 1 ", 1, 1, 10000),
-                new Project ("Meu projeto ASPNET Core 2 ", "Minha descrição de projeto 2 ", 1, 1, 20000),
-                new Project ("Meu projeto ASPNET Core 3 ", "Minha descrição de projeto 3 ", 1, 1, 30000)
-            };
-
-            Users = new List<User>
-            {
-                new User("Matheus Coutinho", "matheuscoutinho@gmail.com.br", new DateTime(2001, 1, 1)),
-                new User("Luis Felipe", "luisfelipe@gmail.com.br", new DateTime(1992, 1, 1)),
-                new User("Anderson", "anderson@gmail.com.br", new DateTime(1998, 1, 1))
-            };
-
-            Skills = new List<Skill>
-            {
-                new Skill(".NET Core"),
-                new Skill("C#"),
-                new Skill("SQL"),
-            };
+           
         }
 
         // Lista Criadas através das referências que foram criadas na pasta Entities no camada Core.
-        public List<Project> Projects { get; set; }
-        public List<User> Users { get; set; }
-        public List<Skill> Skills { get; set; }
-        public List<ProjectComment> ProjectComments { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<UserSkill> UserSkill { get; set; }
+        public DbSet<ProjectComment> ProjectComments { get; set; }
     }
 }
