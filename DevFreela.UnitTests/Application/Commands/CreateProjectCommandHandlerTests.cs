dac6ea93e970fd5a542d20a77a934696623278e7
Matheus <matheuscnc4@gmail.com>
2023-12-18 23:03:38 -0300

    Testes Unitários com xUnit

---
+++ DevFreela.UnitTests/Application/Commands/CreateProjectCommandHandlerTests.cs
@@ -0,0 +1,36 @@
+﻿using DevFreela.Application.Commands.CreateProject;
+using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using Moq;
+
+namespace DevFreela.UnitTests.Application.Commands
+{
+    public class CreateProjectCommandHandlerTests
+    {
+        [Fact]
+        public async Task InputDataIsOK_Executed_returnProjectId ()
+        {
+            // Arrange
+            var projectRepository = new Mock<IProjectRepository>();
+
+            var createProjectCommand = new CreateProjectCommand
+            {
+                Title = "Titulo de Teste",
+                Description = "Descrição",
+                TotalCost = 50000,
+                IdClient = 1,
+                IdFreelancer = 2
+            };
+
+            var createProjectCommandHandler = new CreateProjectCommandHandler(projectRepository.Object);
+
+            // Act
+            var id = await createProjectCommandHandler.Handle(createProjectCommand, new CancellationToken());
+
+            // Assert
+            Assert.True(id >= 0);
+
+            projectRepository.Verify(pr => pr.AddAsync(It.IsAny<Project>()), Times.Once);
+        }
+    }
+}
